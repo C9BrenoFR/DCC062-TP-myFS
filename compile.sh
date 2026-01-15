9f23a159e9c8ca36915b5b775366dd342c02c893
C9BrenoFR <brenufr@gmail.com>
2026-01-15 18:52:33 -0300

    Add compile script to compile and execute test program

---
+++ compile.sh
@@ -0,0 +1,11 @@
+# Lembre de rodar chmod +x compile.sh
+# Esse comando compila, executa e apaga o executavel em seguida.
+
+# Compila
+g++ test.cpp -o main
+
+# Executa (Com parametro)
+./main $1
+
+# Removendo executavel
+rm main 
